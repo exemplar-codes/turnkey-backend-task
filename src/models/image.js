3e94fd960937eb1876590d290be0040b5b30b831
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-17 12:35:39 +0530

    Remove uneeded code

--- src/models/image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const imageSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Image", imageSchema);
